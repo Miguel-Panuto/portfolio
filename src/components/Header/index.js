58db198362aabea3b2eac5e88145586177320626
Miguel Panuto <miguel.panuto@live.com>
2020-03-30 21:21:38 -0300

    responsive design

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link, animateScroll } from 'react-scroll';
 
@@ -9,7 +9,8 @@ import './styles.css';
 
 
 export default function Header() {
-
+  const [isMobile, setIsMobile] = useState(document.body.clientWidth <= 500);
+  const [iconSize, setIconSize] = useState(document.body.clientWidth <= 500 ? 50:100);
   const defaultButtons = () => {
     const previousSelected = document.getElementsByClassName('element selected')[0];
     if (previousSelected)
@@ -21,26 +22,52 @@ export default function Header() {
   }
 
   window.onscroll = () => {
-      const screenPos = window.scrollY/document.body.clientHeight*100;
-      if (screenPos >= 15 && screenPos < 35) {
-        borderBottomHandler('about');
-      } else if (screenPos >= 35 && screenPos < 55) {
-        borderBottomHandler('skills');
-      } else if (screenPos >= 55) {
-        borderBottomHandler('contact');
-      } else {
-        defaultButtons();
-      }
+    if (isMobile) {
+      return
+    }
+    const screenPos = window.scrollY / document.body.clientHeight * 100;
+    if (screenPos >= 15 && screenPos < 35) {
+      borderBottomHandler('about');
+    } else if (screenPos >= 35 && screenPos < 55) {
+      borderBottomHandler('skills');
+    } else if (screenPos >= 55) {
+      borderBottomHandler('contact');
+    } else {
+      defaultButtons();
+    }
+  }
+
+  const RenderHeader = () => {
+    if (!isMobile) {
+      return (
+        <>
+          <ListItem route="about" name="Sobre" />
+          <ListItem route="skills" name="Habilidades" />
+          <ListItem route="contact" name="Contato" />
+        </>
+      )
+    }
+    return <></>;
+  }
+
+  window.onresize = () => {
+    if (document.body.clientWidth <= 500) {
+      setIsMobile(true);
+      setIconSize(50);
+    } else {
+      setIsMobile(false);
+      setIconSize(100);
+    }
   }
   const ListItem = props => {
     const className = "element";
     return (
       <li>
         <Link to={props.route} smooth={true} duration={1000}>
-          <div 
-            className={className} 
-            id={props.route + '-nav'} 
-            onClick={() => borderBottomHandler(props.route) }>
+          <div
+            className={className}
+            id={props.route + '-nav'}
+            onClick={() => borderBottomHandler(props.route)}>
             {props.name}
           </div>
         </Link>
@@ -51,13 +78,11 @@ export default function Header() {
   return (
     <header>
       <div className="logo" onClick={() => { animateScroll.scrollToTop(); defaultButtons(); }}>
-        <img src={logo} alt="Logo" width="100" />
+        <img src={logo} alt="Logo" width={iconSize} />
       </div>
       <nav>
         <ul>
-          <ListItem route="about" name="Sobre" />
-          <ListItem route="skills" name="Habilidades" />
-          <ListItem route="contact" name="Contato" />
+          <RenderHeader />
         </ul>
       </nav>
     </header>
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLinkedin, FiGithub, FiMail } from 'react-icons/fi'
 import './styles.css';
 
 export default function Contact() {
+  const [iconSize, setIconSize] = useState(document.body.clientWidth <= 500 ? 30:50);
+  window.onresize = () => {
+      setIconSize(document.body.clientWidth <= 500 ? 30:50);
+  }
   return (
     <div id="contact" className="page">
       <div className="contact-container">
@@ -12,18 +16,18 @@ export default function Contact() {
         <div className="social-container">
           <a href="https://www.linkedin.com/in/miguel-panuto-b7108b182/">
             <div className="social-item">
-              <FiLinkedin size={50} />
+              <FiLinkedin size={iconSize} />
               <span>Miguel Panuto</span>
             </div>
           </a>
           <a href="https://github.com/Miguel-Panuto">
             <div className="social-item">
-              <FiGithub size={50} />
+              <FiGithub size={iconSize} />
               <span>Miguel-Panuto</span>
             </div>
           </a>
           <div className="social-item">
-            <FiMail size={50} />
+            <FiMail size={iconSize} />
             <input type="email" id="email" value="miguel.panuto@gmail.com" disabled/>
           </div>
         </div>
