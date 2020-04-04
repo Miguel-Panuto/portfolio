2bad1a338e145208903a8577577c090968750533
Miguel Panuto <miguel.panuto@live.com>
2020-04-04 11:22:23 -0300

    Updates

---
+++ precache-manifest.e2f4654229f3b1a1b35c4ca5f5cc0566.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d8fcea782eb51ba34b6ac1a8b541c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "512660958868b47e4a54",
+    "url": "/portfolio/static/css/main.aaa38b49.chunk.css"
+  },
+  {
+    "revision": "f07a03f33c62ac0003ef",
+    "url": "/portfolio/static/js/2.60c9c368.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.60c9c368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512660958868b47e4a54",
+    "url": "/portfolio/static/js/main.857a31fe.chunk.js"
+  },
+  {
+    "revision": "a4ffd17754414a0fa8db",
+    "url": "/portfolio/static/js/runtime-main.33721a5b.js"
+  },
+  {
+    "revision": "b7fa1e30658a82287ed396d5e09f75b4",
+    "url": "/portfolio/static/media/Logo.b7fa1e30.svg"
+  },
+  {
+    "revision": "2ffa82156896170b82b166aba86b4249",
+    "url": "/portfolio/static/media/ProfileImage.2ffa8215.png"
+  },
+  {
+    "revision": "e96d8f07375a4ddb574a8b72440005d5",
+    "url": "/portfolio/static/media/logos.e96d8f07.png"
+  }
+]);
